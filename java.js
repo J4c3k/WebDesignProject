21d38c1c806dcf6b0ebb33d476cd2ef90eb085d7
J4c3k <38157232+J4c3k@users.noreply.github.com>
2018-04-13 18:04:09 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -5,10 +5,44 @@ J Smolen
 */
 //This Java Script is used to add items to the basket.
 
+var items=[];
+var order="";
+var quantity=[];
+var newItem;
+var prices=[];
+var sum=0;
+var summary
 
 
-function addProduct(name){
+function addProduct(name, price){
+	newItem=1;
+	for(i=0;i<items.length;i++){
+		if(name==items[i]){
+			quantity[i]++;
+			newItem=0;
+		}
+	}
 
-	document.getElementById("list").innerHTML = name;
+	if(newItem==1){
+		items.push(name);
+		prices.push(price);
+		quantity.push(1);
+	}
+
+	order="";
+	for(i=0;i<items.length;i++){
+		order+='<li>' +quantity[i] +'x ' + items[i] +' - ' + quantity[i]*prices[i] + '€' + '</li>';
+	}
+
+	document.getElementById("list").innerHTML = order;
 }
 
+function pay(){
+	summary="ITEMS LIST:\n";
+	for(i=0;i<prices.length;i++){
+		sum+=prices[i]*quantity[i];
+		summary+=quantity[i]+"x		"+items[i]+"		"+prices[i]+"€\n";
+	}
+	alert(summary+'Total to pay:		'+sum+ '€');
+
+}
